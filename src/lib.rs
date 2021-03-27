d9ca78b3b482f8f90095084d0e361e28354bd141
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-03-27 15:12:24 +0100

    test: dont check actual base64 string
    only the html structure

--- src/lib.rs
+++ src/lib.rs
@@ -61,7 +61,7 @@ pub fn html_inline(html: &str) -> Result<String, Box<dyn Error>> {
 #[test]
 fn check_img_src_file() {
     let html = r#"<div><img class="something" src="testdata/white-pixel.png"></img></div>"#;
-    let expected = r#"<div><img class="something" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQAAAAA3bvkkAAAAGXRFWHRDb21tZW50AENyZWF0ZWQgd2l0aCBHSU1QV4EOFwAAAYVpQ0NQSUNDIHByb2ZpbGUAACiRfZE9SMNAHMVfU6WiFQc7iDpkqE4WREUEF61CESqEWqFVB5NLP4QmDUmKi6PgWnDwY7Hq4OKsq4OrIAh+gLi5OSm6SIn/SwotYjw47se7e4+7d4BQKzHNahsFNN02U4m4mMmuiKFXBDGALkQwLTPLmJWkJHzH1z0CfL2L8Sz/c3+ObjVnMSAgEs8ww7SJ14knN22D8z5xhBVllficeMSkCxI/cl3x+I1zwWWBZ0bMdGqOOEIsFlpYaWFWNDXiCeKoqumUL2Q8VjlvcdZKFda4J39hOKcvL3Gd5iASWMAiJIhQUMEGSrARo1UnxUKK9uM+/n7XL5FLIdcGGDnmUYYG2fWD/8Hvbq38+JiXFI4D7S+O8zEEhHaBetVxvo8dp34CBJ+BK73pL9eAqU/Sq00tegT0bAMX101N2QMud4C+J0M2ZVcK0hTyeeD9jL4pC/TeAp2rXm+NfZw+AGnqKnkDHBwCwwXKXvN5d0drb/+eafT3A5hMcrbU4IAiAAAACXBIWXMAAC4jAAAuIwF4pT92AAAAB3RJTUUH5QMbCxUpM7cbRgAAAApJREFUeNpjaAAAAIIAgdpFCDsAAAAASUVORK5CYII="></img></div>"#;
     let result = html_inline(html).unwrap();
-    assert_eq!(result, expected);
+    assert!(result.starts_with(r#"<div><img class="something" src="data:image/png;base64,"#));
+    assert!(result.ends_with(r#"="></img></div>"#));
 }
